4aec08bd6182c1eadd15b7b37883e6add1aeffdc
Matthew Leibowitz <mattleibow@live.com>
2024-11-23 01:53:36 +0800

    Update MainActivity.cs

--- MauiAppWithMultipleIcons/Platforms/Android/MainActivity.cs
+++ MauiAppWithMultipleIcons/Platforms/Android/MainActivity.cs
@@ -5,9 +5,14 @@ using Android.Runtime;
 
 namespace MauiAppWithMultipleIcons
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = false, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = EnableMainActivityMainLauncher, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     [Register("com.companyname.mauiappwithmultipleicons.MainActivity")]
     public class MainActivity : MauiAppCompatActivity
     {
+#if DEBUG
+        const bool EnableMainActivityMainLauncher = true;
+#else
+        const bool EnableMainActivityMainLauncher = false;
+#endif
     }
 }
